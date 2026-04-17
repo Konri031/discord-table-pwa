7789868d399d3c16472c09b525625b15d4759b93
Konri031 <124524508+Konri031@users.noreply.github.com>
2026-04-17 05:43:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,2 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
